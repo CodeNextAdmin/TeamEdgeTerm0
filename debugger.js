3237c7170041a7a8d4cca414c1913be8efea4e8e
Code Next <61989260+CodeNextAdmin@users.noreply.github.com>
2020-07-09 10:28:39 -0700

    Update debugger.js
    added solutions

--- debugger.js
+++ debugger.js
@@ -3,23 +3,22 @@ const options = ["rock", "paper", "scissors"];
 
 console.log(`Let's play Rock Paper Scissors!`);
 
-// Challenge
-// Find the bugs below:
 
 while(true){
 	let userInput = READLINE.question(`Do you want to play rock, paper, or scissors? `).toLowerCase();
-	randomSelection = Math.floor(Math.random()*3);
-  computerSelection = options[randomSelection];
+	let randomSelection = Math.floor(Math.random()*3);
+  let computerSelection = options[randomSelection];
 
-	console.log("You played: ${userInput} and the computer played: ${computerSelection}");
+	console.log(`You played: ${userInput} and the computer played: ${computerSelection}`);
   if(userInput === computerSelection){
     console.log("It's a tie!");
   }
-  else if((userInput = "rock" && computerSelection = "paper") ||
-          (userInput = "paper" && computerSelection = "scissors") ||
-          (userInput = "scissors" && computerSelection = "rock")){
+  else if((userInput === "rock" && computerSelection === "paper") ||
+          (userInput === "paper" && computerSelection === "scissors") ||
+          (userInput === "scissors" && computerSelection === "rock")){
     console.log("You Lose!");
   }
-  else if{
+  else{
     console.log("You Win!");
   }
+}
