8c133a71a9ba86d5847c54ea06f96368f4a89677
Code Next <61989260+CodeNextAdmin@users.noreply.github.com>
2020-07-09 10:32:15 -0700

    Update ConditionalGame.js
    added solution/example

--- ConditionalGame.js
+++ ConditionalGame.js
@@ -29,19 +29,10 @@
 
   -------------------------------------------- */
 
+const READLINE = require("readline-sync");
 
-
-
-
-
-
-
-
-
-
-
-
-
+console.log(`Thanks for using the Magic 8 Ball!`);
+let userInput = READLINE.question(`What would you like to know?  `);
 
 /* -------------------------------------------- 
 
@@ -74,42 +65,68 @@
 
   -------------------------------------------- */
 
+let response = Math.floor(Math.random() * 20);
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/* -------------------------------------------- 
-
-	Part 3: Customize it!
-
-	Select your own theme and use case and modify your code!
-	
-  -------------------------------------------- */
-
-
-
-
-
-
-
-
-
-
+if(response == 0){
+	console.log(`It is certain.`);
+}
+else if(response == 1){
+	console.log(`It is decidedly so.`);
+}
+else if(response == 2){
+	console.log(`Without a doubt.`);
+}
+else if(response == 3){
+	console.log(`Yes - definitely.`);
+}
+else if(response == 4){
+	console.log(`You may rely on it.`);
+}
+else if(response == 5){
+	console.log(`As I see it, yes.`);
+}
+else if(response == 6){
+	console.log(`Most likely.`);
+}
+else if(response == 7){
+	console.log(`Outlook good.`);
+}
+else if(response == 8){
+	console.log(`Yes.`);
+}
+else if(response == 9){
+	console.log(`Signs point to yes.`);
+}
+else if(response == 10){
+	console.log(`Reply hazy, try again.`);
+}
+else if(response == 11){
+	console.log(`Ask again later.`);
+}
+else if(response == 12){
+	console.log(`Better not tell you now.`);
+}
+else if(response == 13){
+	console.log(`Cannot predict now.`);
+}
+else if(response == 14){
+	console.log(`Concentrate and ask again.`);
+}
+else if(response == 15){
+	console.log(`Don't count on it.`);
+}
+else if(response == 16){
+	console.log(`My reply is no.`);
+}
+else if(response == 17){
+	console.log(`My sources say no.`);
+}
+else if(response == 18){
+	console.log(`Outlook not so good.`);
+}
+else if(response == 19){
+	console.log(`Very doubtful.`);
+}
 
 
 
