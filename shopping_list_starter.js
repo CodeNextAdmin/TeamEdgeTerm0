09eac7f1d85c3b53280bd8cae624f6671f18bb50
Code Next <61989260+CodeNextAdmin@users.noreply.github.com>
2020-07-09 23:31:43 -0700

    Update shopping_list_starter.js
    added solution

--- shopping_list_starter.js
+++ shopping_list_starter.js
@@ -2,8 +2,8 @@
  *                                                                  
  *  Team Edge Array Mini-project: THE SHOPPING LIST HELPER
  * 
- *  This project prompts users using readline_sync to prompt users
- *  to add (or remove) items from a shopping list. It starts empty
+ *  This project prompts users using readline_sync to add 
+ *  (or remove) items from a shopping list. It starts empty
  *  and each time the program is run it asks you to either add or 
  *  remove an item from the list. It also updates the user of its
  *  contents. The shopping list also checks to see if an item 
@@ -32,13 +32,11 @@ or you can type 'remove milk' to remove it. \n`
 
 console.log(welcomeMessage)
 
-
-//-->Todo: declare a shoppingList array
-
+let shoppingList = []
 
 while(active){
 
-    checkAnswer(promptUser()) //this makes the program continously prompt and check response while the boolean 'active' returns true
+    checkAnswer(promptUser()) //this makes the game continuously prompt and check response
 
 }
 
@@ -50,17 +48,60 @@ function promptUser(){
 
 }
 
-function checkAnswer(){
+function checkAnswer(response){
+
+    let item = response.split(" ")[1] //split the "add milk" command and grab the second word (index 1)
+
+    if(response.includes("add")){
+    
+        console.log("adding: " + item) 
+        addItem(item)
+    
+
+    } else if(response.includes("remove")){
+
+        removeItem(item)
+
+    }else {
+
+        console.log("I didin't understand that. Try typing add/remove followed by an item")
+
+
+    }
+
+}
+
+function addItem(item){
+
+    if(shoppingList.includes(item)){
+
+        console.log("You already added " + item)
+
+    }else {
+
+        shoppingList.push(item)
+        console.log("the shopping list now has: " + shoppingList)
+    }
 
 
 }
 
-function addItem(){
-//this function can take in a string and store it in an array
+function removeItem(item){
+
+    if(shoppingList.includes(item)){
+
+        let itemToRemove = shoppingList.indexOf(item) // indexOf() gets the index of the item we will remofe from the array
+
+        shoppingList.splice(itemToRemove, 1) //removes the item at the given index. 1 means only one item to remove.
+        
+        console.log(`${item} removed. The shopping list now has: ${ shoppingList}`)
+
+
+    }else {
+        console.log(`Can't remove ${item} because it is not in your shopping list`)
+
+    }
 
 }
 
-function removeItem(){
 
-
-}
